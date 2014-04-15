e5e905d12a8a71ebedb4c49a96bd189a4716cb9d
bjzhou <bjzhou@lewatek.com>
2014-04-15 13:57:51 +0800

    remove game and change font

--- src/org/x2ools/wechat/WeChat.java
+++ src/org/x2ools/wechat/WeChat.java
@@ -1,10 +1,25 @@
 package org.x2ools.wechat;
 
+import android.content.Context;
+import android.graphics.Typeface;
+import android.widget.TextView;
 import de.robv.android.xposed.IXposedHookZygoteInit.StartupParam;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
-public class WeChat{
+public class WeChat {
+
+	public static final String PACKAGE_NAME = "com.tencent.mm";
+
+	public static final String CLASS_SETTINGS_ABOUT_SYSTEM_UI = "com.tencent.mm.ui.setting.SettingsAboutSystemUI";
+	public static final String CLASS_MM_TEXTVIEW = "com.tencent.mm.ui.base.MMTextView";
+	public static final String CLASS_FIND_MORE_FRIENDS_UI = "com.tencent.mm.ui.pluginapp.FindMoreFriendsUI";
+	public static final String CLASS_PREFERENCE = "com.tencent.mm.ui.base.preference.Preference";
+	public static final String CLASS_PREFERENCE_TITLE_CATEGORY = "com.tencent.mm.ui.base.preference.PreferenceTitleCategory";
+
+	private static int preference_layout;
 
 	public static void initZygote(StartupParam startupParam) throws Throwable {
 		// TODO Auto-generated method stub
@@ -12,12 +27,66 @@ public class WeChat{
 	}
 
 	public static void handleInitPackageResources(InitPackageResourcesParam resparam) throws Throwable {
-		// TODO Auto-generated method stub
 
+		if (!resparam.packageName.equals(PACKAGE_NAME))
+			return;
+
+		preference_layout = resparam.res.getIdentifier("mm_preference", "layout", PACKAGE_NAME);
 	}
 
-	public static void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
-		// TODO Auto-generated method stub
+	// FIXME only work for wechat version 5.2.1,versioncode 400
+	public static void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+
+		if (!lpparam.packageName.equals(PACKAGE_NAME))
+			return;
+
+		Class<?> settingsAboutSystemUI = XposedHelpers.findClass(CLASS_SETTINGS_ABOUT_SYSTEM_UI, lpparam.classLoader);
+		Class<?> mmTextViewClz = XposedHelpers.findClass(CLASS_MM_TEXTVIEW, lpparam.classLoader);
+		Class<?> findMoreFriendsUI = XposedHelpers.findClass(CLASS_FIND_MORE_FRIENDS_UI, lpparam.classLoader);
+
+		// add font setting in general setting
+		// TODO on click listener
+		XposedHelpers.findAndHookMethod(settingsAboutSystemUI, "Bn", new XC_MethodHook() {
+
+			@Override
+			protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+				Class<?> preferenceClz = XposedHelpers.findClass(CLASS_PREFERENCE, lpparam.classLoader);
+				Class<?> preferenceTitleCategoryClz = XposedHelpers.findClass(CLASS_PREFERENCE_TITLE_CATEGORY,
+						lpparam.classLoader);
+				Object preference = preferenceClz.getConstructor(Context.class).newInstance(param.thisObject);
+				Object preferenceTitleCategory = preferenceTitleCategoryClz.getConstructor(Context.class).newInstance(
+						param.thisObject);
+				Object cFy = XposedHelpers.getObjectField(param.thisObject, "cFy");
+				XposedHelpers.callMethod(preferenceTitleCategory, "setTitle", "Xposed设置");
+				XposedHelpers.callMethod(preference, "setKey", "key_font");
+				XposedHelpers.callMethod(preference, "setTitle", "字体");
+				XposedHelpers.callMethod(preference, "setLayoutResource", preference_layout);
+				XposedHelpers.callMethod(cFy, "b", preferenceTitleCategory);
+				XposedHelpers.callMethod(cFy, "b", preference);
+			}
+		});
+
+		// change chatting font
+		// TODO custom font
+		XposedHelpers.findAndHookMethod(mmTextViewClz, "init", new XC_MethodHook() {
+
+			@Override
+			protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+				TextView tv = (TextView) param.thisObject;
+				tv.setTypeface(Typeface.createFromFile("/sdcard/DroidSansFallback.ttf"));
+			}
+		});
+
+		// remove game plugin
+		// TODO add checkbox preference in general setting
+		XposedHelpers.findAndHookMethod(findMoreFriendsUI, "aNn", new XC_MethodHook() {
+
+			@Override
+			protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+				Object cFy = XposedHelpers.getObjectField(param.thisObject, "cFy");
+				XposedHelpers.callMethod(cFy, "S", "more_tab_game_recommend", true);
+			}
+		});
 
 	}
 

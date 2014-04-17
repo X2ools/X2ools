f81c57a7ba6d94558d8e8b9efe84fc360c7cffa2
bjzhou <bjzhou@lewatek.com>
2014-04-17 10:48:25 +0800

    custom setting

--- src/org/x2ools/X2oolsActivity.java
+++ src/org/x2ools/X2oolsActivity.java
@@ -1,14 +1,115 @@
 package org.x2ools;
 
-import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
-public class X2oolsActivity extends PreferenceActivity{
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.os.Bundle;
+import android.os.Environment;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
+
+public class X2oolsActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener{
+	
+	public static final String KEY_WECHAT_REMOVE_GAME = "wechat_remvoe_game";
+	public static final String KEY_WECHAT_CHAT_FONT = "wechat_chat_font";
+	
+	public static final String ACTION_REMOVE_GAME_CHANGED = "x2ools.action.REMOVE_GAME_CHANGED";
+	public static final String ACTION_CHAT_FONT_CHANGED = "x2ools.action.CHAT_FONT_CHANGED";
+	
+	public static final String X2OOL_DIR = Environment.getExternalStorageDirectory() + "/X2ools/";
+	
+	private SharedPreferences prefs;
+	
+	private JSONObject json;
+	private File jsonFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preference);
+        prefs = getPreferenceScreen().getSharedPreferences();
+        initJsonData();
     }
+
+	@Override
+	protected void onResume() {
+		prefs.registerOnSharedPreferenceChangeListener(this);
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		prefs.unregisterOnSharedPreferenceChangeListener(this);
+		super.onPause();
+	}
+
+	private void initJsonData() {
+		json = new JSONObject();
+		initFile();
+		try {
+			json.put(KEY_WECHAT_CHAT_FONT, URLEncoder.encode(prefs.getString(KEY_WECHAT_CHAT_FONT, ""),"UTF-8"));
+			json.put(KEY_WECHAT_REMOVE_GAME, prefs.getBoolean(KEY_WECHAT_REMOVE_GAME, false));
+		} catch (JSONException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			FileOutputStream fos = new FileOutputStream(jsonFile);
+			fos.write(json.toString().getBytes());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private void initFile() {
+		File x2oolsDir = new File(X2OOL_DIR);
+		if(!x2oolsDir.exists()) {
+			x2oolsDir.mkdir();
+		} 
+		jsonFile = new File(x2oolsDir+"/prefs.json");
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		if(key.equals(KEY_WECHAT_REMOVE_GAME)) {
+			try {
+				json.put(KEY_WECHAT_REMOVE_GAME, sharedPreferences.getBoolean(KEY_WECHAT_REMOVE_GAME, false));
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		} else if(key.equals(KEY_WECHAT_CHAT_FONT)) {
+			try {
+				json.put(KEY_WECHAT_CHAT_FONT, URLEncoder.encode(sharedPreferences.getString(KEY_WECHAT_CHAT_FONT, ""),"UTF-8"));
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if(jsonFile == null)
+			initFile();
+		try {
+			FileOutputStream fos = new FileOutputStream(jsonFile);
+			fos.write(json.toString().getBytes());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
     
 }
--- src/org/x2ools/wechat/WeChat.java
+++ src/org/x2ools/wechat/WeChat.java
@@ -1,11 +1,22 @@
 
 package org.x2ools.wechat;
 
-import android.graphics.Color;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.net.URLDecoder;
+
+import org.json.JSONObject;
+import org.x2ools.X2oolsActivity;
+
 import android.graphics.Typeface;
+import android.os.Environment;
+import android.text.TextUtils;
 import android.widget.TextView;
 import de.robv.android.xposed.IXposedHookZygoteInit.StartupParam;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodHook.MethodHookParam;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
@@ -19,8 +30,9 @@ public class WeChat {
     public static final String CLASS_FIND_MORE_FRIENDS_UI = "com.tencent.mm.ui.pluginapp.FindMoreFriendsUI";
     public static final String CLASS_PREFERENCE = "com.tencent.mm.ui.base.preference.Preference";
     public static final String CLASS_PREFERENCE_TITLE_CATEGORY = "com.tencent.mm.ui.base.preference.PreferenceTitleCategory";
-
-    private static int preference_layout;
+    
+    private static JSONObject json = null;
+    
 
     public static void initZygote(StartupParam startupParam) throws Throwable {
         // TODO Auto-generated method stub
@@ -32,8 +44,7 @@ public class WeChat {
 
         if (!resparam.packageName.equals(PACKAGE_NAME))
             return;
-
-        preference_layout = resparam.res.getIdentifier("mm_preference", "layout", PACKAGE_NAME);
+        
     }
 
     // FIXME only work for wechat version 5.2.1,versioncode 400
@@ -41,63 +52,48 @@ public class WeChat {
 
         if (!lpparam.packageName.equals(PACKAGE_NAME))
             return;
+        
+        File jsonFile = new File(Environment.getExternalStorageDirectory()+"/X2ools/prefs.json");
+        InputStreamReader isr = new InputStreamReader(new FileInputStream(jsonFile));
+        BufferedReader bufferedReader = new BufferedReader(isr);
+        String receiveString = "";
+        StringBuilder stringBuilder = new StringBuilder();
 
-        Class<?> settingsAboutSystemUI = XposedHelpers.findClass(CLASS_SETTINGS_ABOUT_SYSTEM_UI,
-                lpparam.classLoader);
+        while ( (receiveString = bufferedReader.readLine()) != null ) {
+            stringBuilder.append(receiveString);
+        }
+
+        if(!TextUtils.isEmpty(stringBuilder))
+        	json = new JSONObject(stringBuilder.toString());
+
         Class<?> mmTextViewClz = XposedHelpers.findClass(CLASS_MM_TEXTVIEW, lpparam.classLoader);
         Class<?> findMoreFriendsUI = XposedHelpers.findClass(CLASS_FIND_MORE_FRIENDS_UI,
                 lpparam.classLoader);
 
-        // add font setting in general setting
-        // TODO on click listener
-        /*
-        XposedHelpers.findAndHookMethod(settingsAboutSystemUI, "Bn", new XC_MethodHook() {
-
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                Class<?> preferenceClz = XposedHelpers.findClass(CLASS_PREFERENCE,
-                        lpparam.classLoader);
-                Class<?> preferenceTitleCategoryClz = XposedHelpers.findClass(
-                        CLASS_PREFERENCE_TITLE_CATEGORY,
-                        lpparam.classLoader);
-                Object preference = preferenceClz.getConstructor(Context.class).newInstance(
-                        param.thisObject);
-                Object preferenceTitleCategory = preferenceTitleCategoryClz.getConstructor(
-                        Context.class).newInstance(
-                        param.thisObject);
-                Object cFy = XposedHelpers.getObjectField(param.thisObject, "cFy");
-                XposedHelpers.callMethod(preferenceTitleCategory, "setTitle", "Xposed设置");
-                XposedHelpers.callMethod(preference, "setKey", "key_font");
-                XposedHelpers.callMethod(preference, "setTitle", "字体");
-                XposedHelpers.callMethod(preference, "setLayoutResource", preference_layout);
-                XposedHelpers.callMethod(cFy, "b", preferenceTitleCategory);
-                XposedHelpers.callMethod(cFy, "b", preference);
-            }
-        });
-        */
-
         // change chatting font
-        // TODO custom font
         XposedHelpers.findAndHookMethod(mmTextViewClz, "init", new XC_MethodHook() {
 
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                 TextView tv = (TextView) param.thisObject;
-                tv.setTypeface(Typeface.createFromFile("/sdcard/DroidSansFallback.ttf"));
-                //tv.setTextColor(Color.BLUE);
-                tv.setTextSize(20);
+                if(json != null) {
+                	String font = URLDecoder.decode((String) json.get(X2oolsActivity.KEY_WECHAT_CHAT_FONT), "UTF-8");
+                	if(!TextUtils.isEmpty(font))
+                		tv.setTypeface(Typeface.createFromFile(font));	
+                }
             }
         });
 
         // remove game plugin
-        // TODO add checkbox preference in general setting
-        
         XposedHelpers.findAndHookMethod(findMoreFriendsUI, "aNn", new XC_MethodHook() {
 
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                 Object cFy = XposedHelpers.getObjectField(param.thisObject, "cFy");
-                XposedHelpers.callMethod(cFy, "S", "more_tab_game_recommend", true);
+                if(json != null) {
+                	boolean canRemove = json.getBoolean(X2oolsActivity.KEY_WECHAT_REMOVE_GAME);
+                	XposedHelpers.callMethod(cFy, "S", "more_tab_game_recommend", canRemove);
+                }
             }
         });
         

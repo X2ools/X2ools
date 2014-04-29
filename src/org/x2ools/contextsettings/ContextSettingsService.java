3a9d222d6f54f836513ae6175940d884e63a7d2e
bjzhou <bjzhou@lewatek.com>
2014-04-29 11:56:16 +0800

    optimize float window

--- src/org/x2ools/contextsettings/ContextSettingsService.java
+++ src/org/x2ools/contextsettings/ContextSettingsService.java
@@ -1,3 +1,4 @@
+
 package org.x2ools.contextsettings;
 
 import android.app.Service;
@@ -10,6 +11,7 @@ import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.WindowManager;
 import android.view.WindowManager.LayoutParams;
@@ -17,10 +19,10 @@ import android.view.WindowManager.LayoutParams;
 import org.x2ools.R;
 import org.x2ools.X2oolsActivity;
 
-public class ContextSettingsService extends Service implements ContextSettingsView.CallBack{
+public class ContextSettingsService extends Service implements ContextSettingsView.CallBack {
     private static final String TAG = "ContextSettingsService";
     private ContextSettingsView mParentView;
-    private WindowManager mWindowManager; 
+    private WindowManager mWindowManager;
     public static String ACTION_CONTEXT_SETTINGS = "ACTION_CONTEXT_SETTINGS";
     public static String KEY_ENABLE = "enable";
 
@@ -28,84 +30,89 @@ public class ContextSettingsService extends Service implements ContextSettingsVi
     public IBinder onBind(Intent arg0) {
         return null;
     }
+
     private LayoutParams mLayoutParams;
+
     @Override
     public void onCreate() {
         IntentFilter filter = new IntentFilter(ACTION_CONTEXT_SETTINGS);
         registerReceiver(mReceiver, filter);
-        mWindowManager = (WindowManager)getSystemService(Context.WINDOW_SERVICE);
+        mWindowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-        if(sp.getBoolean(X2oolsActivity.KEY_CONTEXT_SETTINGS, true)) {
+        if (sp.getBoolean(X2oolsActivity.KEY_CONTEXT_SETTINGS, true)) {
             addView();
         }
         super.onCreate();
     }
-    
+
     @Override
     public void onDestroy() {
         super.onDestroy();
         removeView();
     }
-    
+
     public void addView() {
-        if(mParentView == null || mLayoutParams == null) {
-            mParentView = (ContextSettingsView)LayoutInflater.from(this).inflate(R.layout.context_settings, null).findViewById(R.id.contextSettingsView);
+        if (mParentView == null || mLayoutParams == null) {
+            mParentView = (ContextSettingsView) LayoutInflater.from(this)
+                    .inflate(R.layout.context_settings, null)
+                    .findViewById(R.id.contextSettingsView);
             mParentView.setCallBack(this);
             mLayoutParams = new LayoutParams();
-            mLayoutParams.y = 40;
-            mLayoutParams.x = 500;
+            mLayoutParams.y = mWindowManager.getDefaultDisplay().getHeight()/2;
+            mLayoutParams.x = mWindowManager.getDefaultDisplay().getWidth();
+            mLayoutParams.gravity = Gravity.TOP | Gravity.LEFT;
             mLayoutParams.width = LayoutParams.WRAP_CONTENT;
             mLayoutParams.height = LayoutParams.WRAP_CONTENT;
             mLayoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
-            
-            mLayoutParams.flags = 
-                       LayoutParams.FLAG_NOT_TOUCH_MODAL
-                      |LayoutParams.FLAG_NOT_FOCUSABLE;
-            
+
+            mLayoutParams.flags =
+                    LayoutParams.FLAG_NOT_TOUCH_MODAL
+                            | LayoutParams.FLAG_NOT_FOCUSABLE;
+
             mLayoutParams.format = PixelFormat.TRANSLUCENT;
         }
-        if(mParentView.getWindowToken() == null) {
+        if (mParentView.getWindowToken() == null) {
             mWindowManager.addView(mParentView, mLayoutParams);
         }
     }
-    
+
     public void removeView() {
         Log.d(TAG, "removeView " + mParentView + mParentView.getWindowToken());
-        if(mParentView != null && mParentView.getWindowToken() != null) {
+        if (mParentView != null && mParentView.getWindowToken() != null) {
             mWindowManager.removeView(mParentView);
         }
     }
-    
+
     BroadcastReceiver mReceiver = new BroadcastReceiver() {
 
         @Override
         public void onReceive(Context context, Intent intent) {
             boolean enable = intent.getBooleanExtra(KEY_ENABLE, true);
             Log.d(TAG, "onReceive " + enable);
-            if(enable) {
+            if (enable) {
                 addView();
             }
             else {
                 removeView();
             }
         }
-        
-    };
-    
 
+    };
 
     @Override
     public void onMoved(int dx, int dy) {
-        mLayoutParams.x = mLayoutParams.x + (int)dx;
-        mLayoutParams.y = mLayoutParams.y + (int)dy;
-        Log.d(TAG, "updateViewLayout : "+ mLayoutParams.x + " , " + mLayoutParams.y);
+        mLayoutParams.x = (int) dx;
+        mLayoutParams.y = (int) dy;
+        Log.d(TAG, "updateViewLayout : " + mLayoutParams.x + " , " + mLayoutParams.y);
         mWindowManager.updateViewLayout(mParentView, mLayoutParams);
     }
+
     public static final boolean FOCUSABLE_IN_EXPAND_MODE = false;
+
     @Override
     public void onModeChanged(boolean expand) {
-        if(FOCUSABLE_IN_EXPAND_MODE) {
-            if(expand) {
+        if (FOCUSABLE_IN_EXPAND_MODE) {
+            if (expand) {
                 mLayoutParams.flags &= ~LayoutParams.FLAG_NOT_FOCUSABLE;
             }
             else {
--- src/org/x2ools/contextsettings/ContextSettingsView.java
+++ src/org/x2ools/contextsettings/ContextSettingsView.java
@@ -1,3 +1,4 @@
+
 package org.x2ools.contextsettings;
 
 import android.app.ActivityManager;
@@ -6,7 +7,6 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.Uri;
-import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.AttributeSet;
@@ -17,16 +17,17 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
-import org.x2ools.R;
-
 import com.juuda.droidmock.adb.SettingsMocker;
 
-import java.lang.ref.WeakReference;
+import org.x2ools.R;
 
-public class ContextSettingsView extends FrameLayout{
-	public static final int MESSAGE_UPDATE_VIEW = 0;
-	public static final int MESSAGE_SHOW_BIG = 1;
-	public static final int MESSAGE_SHOW_SMALL = 2;
+import java.lang.ref.WeakReference;
+import java.lang.reflect.Field;
+
+public class ContextSettingsView extends FrameLayout {
+    public static final int MESSAGE_UPDATE_VIEW = 0;
+    public static final int MESSAGE_SHOW_BIG = 1;
+    public static final int MESSAGE_SHOW_SMALL = 2;
 
     private static final String TAG = "ContextSettingsView";
     private TextView mTextCurrentPackage;
@@ -40,18 +41,18 @@ public class ContextSettingsView extends FrameLayout{
     public static final int DELAY_MILLS = 1000;
     private ActivityManager mAm;
     private PackageManager mPm;
-    
+
     public ContextSettingsView(Context context, AttributeSet attrs) {
         super(context, attrs);
         mContext = getContext();
-        mAm = (ActivityManager)mContext.getSystemService(Context.ACTIVITY_SERVICE);
+        mAm = (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
         mPm = mContext.getPackageManager();
     }
 
     @Override
     protected void onFinishInflate() {
-        mTextCurrentPackage = (TextView)findViewById(R.id.currentPackage);
-        mButtonSettings = (Button)findViewById(R.id.gotoSettings);
+        mTextCurrentPackage = (TextView) findViewById(R.id.currentPackage);
+        mButtonSettings = (Button) findViewById(R.id.gotoSettings);
         mLayoutSmall = findViewById(R.id.layoutSmall);
         mLayoutBig = findViewById(R.id.layoutBig);
         mX2ools = findViewById(R.id.x2ools);
@@ -64,42 +65,42 @@ public class ContextSettingsView extends FrameLayout{
     private float mStartY;
     private float mEndX;
     private float mEndY;
-    
+    private float mTouchX;
+    private float mTouchY;
+
     public void setCallBack(CallBack callBack) {
         mCallBack = callBack;
     }
+
     @Override
     public boolean onTouchEvent(MotionEvent motionEvent) {
 
+        mTouchX = motionEvent.getRawX();
+        mTouchY = motionEvent.getRawY() - getStatusBarHeight(mContext);
+
         final int action = motionEvent.getAction();
-        switch(action) {
+        switch (action) {
             case MotionEvent.ACTION_DOWN:
                 mMoved = false;
                 mStartX = motionEvent.getX();
                 mStartY = motionEvent.getY();
-                Log.d(TAG, "mStartX : "+ mStartX + "  mStartY :" + mStartY + "  moved: " + mMoved);
                 break;
             case MotionEvent.ACTION_MOVE:
                 mEndX = motionEvent.getX();
                 mEndY = motionEvent.getY();
-                mMoved = true;
-                Log.d(TAG, "moved mEndX : "+ mEndX + "  mEndY :" + mEndY +  "  moved: " + mMoved);
-                mCallBack.onMoved((int)(mEndX - mStartX), (int)(mEndY - mStartY));
+                if (Math.abs(mEndX - mStartX) > 5 || Math.abs(mEndY - mStartY) > 5) {
+                    mMoved = true;
+                    mCallBack.onMoved((int) (mTouchX - mStartX), (int) (mTouchY - mStartY));
+                }
                 break;
             case MotionEvent.ACTION_UP:
-                if(mMoved) {
-                    mEndX = motionEvent.getX();
-                    mEndY = motionEvent.getY();
-                    Log.d(TAG, "mEndX : "+ mEndX + "  mEndY :" + mEndY);
-                    mCallBack.onMoved((int)(mEndX - mStartX), (int)(mEndY - mStartY));
-                }
-                else {
+                if (!mMoved) {
                     Log.d(TAG, "not Moved");
-                    if(mLayoutSmall.getVisibility() == View.VISIBLE) {
-                    	showBig();
+                    if (mLayoutSmall.getVisibility() == View.VISIBLE) {
+                        showBig();
                     }
                     else {
-                    	showSmall();
+                        showSmall();
                     }
                 }
                 break;
@@ -107,79 +108,96 @@ public class ContextSettingsView extends FrameLayout{
         super.onTouchEvent(motionEvent);
         return true;
     }
-    
+
+    private int getStatusBarHeight(Context context) {
+        Class<?> c = null;
+        Object object = null;
+        Field field = null;
+        int heightInDip = 0;
+        int heightInPixel = 0;
+        try {
+            c = Class.forName("com.android.internal.R$dimen");
+            object = c.newInstance();
+            field = c.getField("status_bar_height");
+            heightInDip = Integer.parseInt(field.get(object).toString());
+            if (context != null)
+                heightInPixel = context.getResources().getDimensionPixelSize(heightInDip);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return heightInPixel;
+    }
+
     private void showSmall() {
-        if(mLayoutSmall.getVisibility() == View.GONE) {
+        if (mLayoutSmall.getVisibility() == View.GONE) {
             mLayoutSmall.setVisibility(View.VISIBLE);
             mLayoutBig.setVisibility(View.GONE);
             mCallBack.onModeChanged(true);
         }
     }
-    
+
     private void showBig() {
-        if(mLayoutSmall.getVisibility() == View.VISIBLE) {
+        if (mLayoutSmall.getVisibility() == View.VISIBLE) {
             mLayoutSmall.setVisibility(View.GONE);
             mLayoutBig.setVisibility(View.VISIBLE);
             mCallBack.onModeChanged(true);
         }
     }
-    
+
     public void updateView() {
-     
+
         mButtonSettings.setOnClickListener(mClickListener);
-        
+
         mX2ools.setOnClickListener(mClickListener);
         mToggleAdb.setOnClickListener(mClickListener);
         sHandler.removeMessages(0);
         sHandler.sendEmptyMessageDelayed(0, DELAY_MILLS);
-        
+
     }
-    
-    
+
     OnClickListener mClickListener = new OnClickListener() {
 
         @Override
         public void onClick(View v) {
-        	showSmall();
-        	if(v == mToggleAdb) {
-        		new Thread() {
-        			public void run() {
-                		new SettingsMocker(mContext, null).toggleAdb();
-        			}
-        		}.start();
-        	}
-        	else if(v == mX2ools) {
+            showSmall();
+            if (v == mToggleAdb) {
+                new Thread() {
+                    public void run() {
+                        new SettingsMocker(mContext, null).toggleAdb();
+                    }
+                }.start();
+            }
+            else if (v == mX2ools) {
                 Intent i = mPm.getLaunchIntentForPackage("org.x2ools");
                 i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 mContext.startActivity(i);
-        	}
-        	else if(v == mButtonSettings) {
+            }
+            else if (v == mButtonSettings) {
                 final String currentPackage = getRunningPackage().toString();
                 final String applicationName = getApplicationName(currentPackage);
-                mTextCurrentPackage.setText(currentPackage + "\n" + applicationName);   
+                mTextCurrentPackage.setText(currentPackage + "\n" + applicationName);
                 Intent i = new Intent();
                 i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 i.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
-                i.setData(Uri.parse("package:"+ currentPackage));
+                i.setData(Uri.parse("package:" + currentPackage));
                 mContext.startActivity(i);
-        	}
+            }
         }
     };
-    
+
     private CharSequence getRunningPackage() {
-        return  mAm.getRunningTasks(1).get(0).topActivity.getPackageName();
+        return mAm.getRunningTasks(1).get(0).topActivity.getPackageName();
     }
-    
+
     private String getApplicationName(final String packageName) {
         try {
-            return (String)mPm.getApplicationLabel(mPm.getApplicationInfo(packageName, 0));
-        }
-        catch (NameNotFoundException e) {
+            return (String) mPm.getApplicationLabel(mPm.getApplicationInfo(packageName, 0));
+        } catch (NameNotFoundException e) {
             e.printStackTrace();
         }
         return null;
     }
-    
+
     @Override
     protected void onAttachedToWindow() {
         sHandler.setView(this);
@@ -190,31 +208,33 @@ public class ContextSettingsView extends FrameLayout{
     static UpdateHandler sHandler = new UpdateHandler();
 
     static class UpdateHandler extends Handler {
-		private WeakReference<ContextSettingsView> mView;
-        
+        private WeakReference<ContextSettingsView> mView;
+
         public void setView(ContextSettingsView view) {
             mView = new WeakReference<ContextSettingsView>(view);
         }
+
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            switch(msg.what) {
-            case MESSAGE_UPDATE_VIEW:
-            	mView.get().updateView();
-            	break;
-            case MESSAGE_SHOW_BIG:
-            	mView.get().showBig();
-            	break;
-            case MESSAGE_SHOW_SMALL:
-            	mView.get().showSmall();
-            	break;
+            switch (msg.what) {
+                case MESSAGE_UPDATE_VIEW:
+                    mView.get().updateView();
+                    break;
+                case MESSAGE_SHOW_BIG:
+                    mView.get().showBig();
+                    break;
+                case MESSAGE_SHOW_SMALL:
+                    mView.get().showSmall();
+                    break;
             }
-            
+
         }
-    } 
-    
+    }
+
     public static interface CallBack {
         public void onMoved(int dx, int dy);
+
         public void onModeChanged(boolean expand);
     }
 }

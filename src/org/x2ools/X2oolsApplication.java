68f251539f0ba62b3e9202a307e5ab50b8d1d001
juude <juude.song@gmail.com>
2014-07-09 14:15:31 +0800

    get path from onCreate

--- src/org/x2ools/X2oolsApplication.java
+++ src/org/x2ools/X2oolsApplication.java
@@ -15,13 +15,16 @@ import java.io.File;
 public class X2oolsApplication extends Application {
 
     // private static final String TAG = "X2oolsApplication";
-    public static final String X2OOLS_DIR = Environment.getExternalStorageDirectory() + "/X2ools/";
+    public static String X2OOLS_DIR = Environment.getExternalStorageDirectory() + "/X2ools/";
 
-    public static final String X2OOLS_PREFS = Environment.getExternalStorageDirectory()
+    public static String X2OOLS_PREFS = Environment.getExternalStorageDirectory()
             + "/X2ools/" + "prefs.json";
 
     @Override
     public void onCreate() {
+        X2OOLS_DIR = Environment.getExternalStorageDirectory() + "/X2ools/";
+        X2OOLS_PREFS = Environment.getExternalStorageDirectory()
+                + "/X2ools/" + "prefs.json";
         initX2oolsDir();
         Mocks.sModuleMap.put("viewdebug", ViewDebugMocker.class);
         startService(new Intent(this, ContextSettingsService.class));
